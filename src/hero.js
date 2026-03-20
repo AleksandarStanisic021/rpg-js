91a87f19b5283ae941861088cfe3dad4630c7cd3
ciandre1976 <ciandre23@gmail.com>
2026-03-20 20:41:46 +0100

    input

--- src/hero.js
+++ src/hero.js
@@ -1,7 +1,14 @@
 import { GameObject } from "./gameObject.js";
 
 export class Hero extends GameObject {
-  constructor({ position }) {
-    super({ position });
+  constructor({ game, sprite, position, scale }) {
+    super({ game, sprite, position, scale });
+  }
+  update() {
+    if (this.game.input.keys.indexOf("ArrowUp") > -1) {
+      this.position.y -= 0.1;
+    } else if (this.game.input.keys.indexOf("ArrowDown") > -1) {
+      this.position.y += 0.1;
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -8,19 +8,33 @@ export const COLS = 15;
 export const ROWS = 20;
 export const GAME_WIDTH = TILE_SIZE * COLS;
 export const GAME_HEIGHT = TILE_SIZE * ROWS;
+
 let canvas = document.getElementById("canvas1");
 let ctx = canvas.getContext("2d");
 
 canvas.width = GAME_WIDTH;
 canvas.height = GAME_HEIGHT;
 
-let world = new World();
-world.drawGrid(ctx);
+class Game {
+  constructor() {
+    this.world = new World();
+    this.input = new Input();
+    this.hero = new Hero({ game: this, position: { x: 3, y: 7 } });
+  }
+  update() {}
 
-let input = new Input();
-console.log(input.keys);
+  render(ctx) {
+    this.world.drawGrid(ctx);
+    this.hero.update();
+    this.hero.draw(ctx);
+  }
+}
 
-const h = new Hero({
-  position: { x: 3, y: 7 },
-});
-h.draw(ctx);
+const game = new Game();
+
+function animate(timestamp) {
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  game.render(ctx);
+  requestAnimationFrame(animate);
+}
+animate(0);
